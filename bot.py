ed0e2e7807e2a2f9723bfb4de42e826d07997e78
Michael French <mifrenc@siue.edu>
2021-08-30 14:48:56 -0500

    bot can handle more events

--- bot.py
+++ bot.py
@@ -13,4 +13,15 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_member_join(member):
+    print(f'{member.name} has joined the Discord server!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    print(f'{message.author} has sent a message in the discord')
+
 client.run(TOKEN)
