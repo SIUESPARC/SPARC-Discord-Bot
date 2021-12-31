be94c7cf3f2dfb503cb7dba28718dc809906533d
frenchytheasian <michael.x.french@gmail.com>
2021-12-31 01:08:16 -0500

    Updated docstrings and added heroku files

--- bot.py
+++ bot.py
@@ -17,34 +17,49 @@ CHANNELS = os.getenv('CHANNELS')        # CHANNELS will hold a list of
 
 client = discord.Client()
 
-def send_groupme(message: discord.message.Message) -> None:
-    """
-    Receive a discord message and send a post request with it to GroupMe
+def send_groupme(message: str) -> None:
+    """Receive a message and copy message into groupMe
+
+    Args:
+        message (str): Message to be sent to GroupMe
     """
 
     url = 'https://api.groupme.com/v3/bots/post'
     data = {
         'bot_id' : GROUPME,
-        'text' : message.content,
+        'text' : message,
         }
     r = requests.post(url, json=data)
     print(r.status_code, r.reason)
 
-def sanitize(message: discord.message.Message) -> discord.message.Message:
+def tweet(message: str) -> None:
+    """Receive a message and tweet message to Twitter account
+
+    Args:
+        message (str): Message to be tweeted
     """
-    Un-Discordize the message by removing any channel or role tags to 
+    pass
+
+def sanitize(message: discord.message.Message) -> str:
+    """Un-Discordize the message by removing any channel or role tags to 
     make the message readable in GroupMe
+
+    Args:
+        message (discord.message.Message): Discord Message with tags
+
+    Returns:
+        str: Message with discord tags converted to text
     """
-    message.content = message.content.replace()
-    return message
+    print(f"Message before: {message.content}")
+
+    sanitized = f"{message.author.display_name}: {message.clean_content}"
+
+    return sanitized
 
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-
-
-
 
 @client.event
 async def on_message(message):
@@ -55,9 +70,6 @@ async def on_message(message):
     if str(message.channel.id) in CHANNELS:
         message = sanitize(message)
         send_groupme(message)
-    else:
-        print("Invalid Channel")
-        await message.channel.send(message.content)
 
 client.run(TOKEN)
 
