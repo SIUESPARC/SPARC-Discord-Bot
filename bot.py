06a37a406f9d13626ae736d52ec0db2ba49ddaf5
frenchytheasian <michael.x.french@gmail.com>
2021-12-31 23:41:12 -0500

    Integrated bot to Twitter Account

--- bot.py
+++ bot.py
@@ -1,21 +1,31 @@
 # bot.py
 import os
-
 import requests
-
+from pprint import pprint
 
 import discord
+import tweepy
 from dotenv import load_dotenv
 
-
-# Get necessary credentials from .env file
+# Get discord and GroupMe credentials
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GROUPME = os.getenv('GROUPME_BOT_ID')
-CHANNELS = os.getenv('CHANNELS')        # CHANNELS will hold a list of 
-                                        # channel ids 
+CHANNELS = os.getenv('DISCORD_CHANNELS')        # CHANNELS will hold a list of channel ids 
 
+# Get twitter credentials
+CONSUMER_KEY = os.getenv('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('TWITTER_CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+# Create Discord Client
 client = discord.Client()
+
+# Authenticate and connect to twitter API
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 def send_groupme(message: str) -> None:
     """Receive a message and copy message into groupMe
@@ -30,7 +40,7 @@ def send_groupme(message: str) -> None:
         'text' : message,
         }
     r = requests.post(url, json=data)
-    print(r.status_code, r.reason)
+    print(f"GroupMe Status: {r.status_code}, {r.reason}")
 
 def tweet(message: str) -> None:
     """Receive a message and tweet message to Twitter account
@@ -38,7 +48,8 @@ def tweet(message: str) -> None:
     Args:
         message (str): Message to be tweeted
     """
-    pass
+    status = api.update_status(status=message)
+    pprint(f"Tweet Status: {status}")
 
 def sanitize(message: discord.message.Message) -> str:
     """Un-Discordize the message by removing any channel or role tags to 
@@ -50,8 +61,6 @@ def sanitize(message: discord.message.Message) -> str:
     Returns:
         str: Message with discord tags converted to text
     """
-    print(f"Message before: {message.content}")
-
     sanitized = f"{message.author.display_name}: {message.clean_content}"
 
     return sanitized
@@ -66,10 +75,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-
     if str(message.channel.id) in CHANNELS:
         message = sanitize(message)
-        send_groupme(message)
+        # send_groupme(message)
+        tweet(message)
 
-client.run(TOKEN)
+if __name__ == "__main__":
+    client.run(TOKEN)
 
