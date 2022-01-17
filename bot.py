9b89e1d20422f54280f79bed6f9ed963d4e8dec2
frenchytheasian <michael.x.french@gmail.com>
2022-01-17 15:25:39 -0600

    Created some beginner bot commands

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import requests
 from pprint import pprint
 
 import discord
+from discord.ext import commands
 import tweepy
 from dotenv import load_dotenv
 
@@ -19,14 +20,13 @@ CONSUMER_SECRET = os.getenv('TWITTER_CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
-# Create Discord Client
-client = discord.Client()
-
 # Authenticate and connect to twitter API
 twitter_client = tweepy.Client( consumer_key=CONSUMER_KEY,
                                 consumer_secret=CONSUMER_SECRET,
                                 access_token=ACCESS_TOKEN,
                                 access_token_secret=ACCESS_TOKEN_SECRET)
+
+bot = commands.Bot(command_prefix='!')
 
 def send_groupme(message: str) -> None:
     """Receive a message and copy message into groupMe
@@ -54,19 +54,34 @@ def tweet(message: str) -> None:
     response = twitter_client.create_tweet(text=message)
     pprint(f"Tweet Status: {response}")
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-@client.event
+
+# Events
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity = discord.Game('Minecraft'))
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if str(message.channel.id) in CHANNELS:
         send_groupme(message)
         tweet(message)
 
+    await bot.process_commands(message)
+
+# Commands
+@bot.command(name="ping", pass_context=True)
+async def calendar(ctx):
+    await ctx.send('pong')
+
+@bot.command(name="playing", pass_context=True)
+async def playing(ctx, game):
+    await bot.change_presence(activity = discord.Game(game))
+
 if __name__ == "__main__":
-    client.run(TOKEN)
+    bot.run(TOKEN)
 
